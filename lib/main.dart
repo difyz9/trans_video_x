41bfc85ac88922d7919054d466e41f7a66be124f
difyz <15564773+difyz@user.noreply.gitee.com>
2025-05-20 16:01:40 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:trans_video_x/core/config/env.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async{
 
     WidgetsFlutterBinding.ensureInitialized();
   
-    await Env.init();
-
+ await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
